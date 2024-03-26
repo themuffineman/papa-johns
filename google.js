21921f8f8bf341aba8a339443ac8d143f2f985fa
Petrus <petrusheya@gmail.com>
2024-03-26 12:46:04 +0200

    while loop for scraping other pages

--- google.js
+++ google.js
@@ -47,11 +47,20 @@ app.get('/', async(req, res) => {
     
         // Wait for cards to load
         await page.waitForSelector('div.rgnuSb.xYjf2e');
+        await page.waitForSelector('.AIYI7d');
         broadcast('Cards selector has Loaded') 
         console.log('Cards selector has Loaded')
     
         // Get all the business cards
         const cards = await page.$$('div[jsname="gam5T"]');
+        
+        // Use the regular expression to extract the last number
+        const textContent = await page.$eval('.AIYI7d', element => element.textContent);
+        const lastNumbersMatch = textContent.match(/\d+$/);
+        const lastNumber = lastNumbersMatch ? parseInt(lastNumbersMatch[0], 10) : null;
+
+        const pagesToScrape = (Math.floor(lastNumber / 20))*20
+        let pageNumber = 20
         const businessData = [];
     
     
@@ -112,13 +121,89 @@ app.get('/', async(req, res) => {
             }  
         }
 
+        while(pageNumber < pagesToScrape){
+
+            await page.goto(`https://www.google.com/localservices/prolist?g2lbs=AIQllVxKz861QWvdO5Yk-8jN09vXU1BbEBmkh2Hm2qFZiXXQ4hcyDxFz7cpcWpincZ-7PEnbuHBsIxferaGVGQgklsLT-u8Wjr7UEh4SXkyXz1reSiOAv3B8EPhD8UPRYAD2jEVvB8aM&hl=en-NA&gl=na&cs=1&ssta=1&q=architects%20in%20texas&oq=architects%20in%20texas&slp=MgA6HENoTUk5dVNhdk1tUmhRTVZnNVZRQmgyd2JRWjNSAggCYACSAZ0CCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXRkaDQ4aDMKCy9nLzF3eWM0cm1kCgsvZy8xdGtzajVkdwoML2cvMTJxaDl3OGZkCg0vZy8xMWc2bmwwbGY1CgsvZy8xdHQxdDJubgoLL2cvMXRobDE4MHMKCy9nLzF0ZGNnc3Y0CgsvZy8xdGc3c2RmNwoNL2cvMTFjbXk0bjl2NAoLL2cvMXRkNGR6cTEKDS9nLzExYndxYmswd2YKCy9nLzF0ZnNuZDRfCg0vZy8xMWI3bHBtOGIxCgsvZy8xdHp6dng1bAoLL2cvMXRteTVnc2gKCy9nLzF0a2I1aGgwCg0vZy8xMWJ4OGNteHM4EgQSAggBEgQKAggBmgEGCgIXGRAA&src=2&serdesk=1&sa=X&ved=2ahUKEwje6ZS8yZGFAxVdRkEAHcLfDt8QjGp6BAgmEAE&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0&lci=${pageNumber}`);
+            broadcast(`Google GMB page ${pageNumber} navigated`);
+            console.log("Google GMB Page Navigated");
+    
+            // Wait for cards to load
+            await page.waitForSelector('div.rgnuSb.xYjf2e');
+            broadcast(`Cards selector for page ${pageNumber} has Loaded`) 
+            console.log('Cards selector has Loaded')
+        
+            // Get all the business cards
+            const cards = await page.$$('div[jsname="gam5T"]');
+            const businessData = [];
+        
+        
+        
+
+            for (const card of cards) {
+                const businessName = await card.$eval('div.rgnuSb.xYjf2e', node => node.textContent);
+                const websiteATag = await card.$('a[aria-label="Website"]');
+                const url = websiteATag ? await (await websiteATag.getProperty('href')).jsonValue() : null;
+        
+                if (url) {
+                    const newPage = await browser.newPage();
+                    console.log('created a page for:', url);
+                    broadcast('created a page for:', url);
+                    newPage.setDefaultNavigationTimeout(900000);
+                    newPage.setDefaultTimeout(900000);
+                
+                    try {
+                        await newPage.goto(url);
+                        console.log(`Navigated to ${url}`);
+                        broadcast(`Navigated to ${url}`);
+
+                        let tempEmails = []
+                        const crawledEmails = await crawl(newPage);
+                        console.log(`Found these emails: ${crawledEmails}`);
+                        broadcast(`Found ${crawledEmails.length} emails`);
+                        tempEmails.push(...crawledEmails)
+
+                        const rootUrl = new URL(url).hostname.replace(/^www\./, '');;
+                        console.log('heres the root url', rootUrl)
+
+                        const anchorTags = await newPage.$$eval('a', anchors => anchors.map(anchor => anchor.href));
+                        console.log('we found these a tags', anchorTags)
+                        const filteredAnchorTags = [...new Set(anchorTags)]; // removing duplicates
+                        console.log('we removed duplicates', filteredAnchorTags)
+                        const internalAnchorTags = filteredAnchorTags.filter((anchor)=> (anchor.includes(rootUrl))) // removing non root urls
+                        console.log('we removed the non root domains', internalAnchorTags)
+                        const internalLinks = internalAnchorTags.filter(link => (link.includes('contact'))); // filtering down to contact links
+                        console.log('only contact links', internalLinks)
+                        
+                        for (const link of internalLinks) {
+                            console.log(`Visiting internal link: ${link}`);
+                            broadcast(`Visiting internal link: ${link}`);
+                            await newPage.goto(link);
+                            const secondaryCrawledEmails = await crawl(newPage);
+                            console.log(`Found these emails on ${link}: ${secondaryCrawledEmails}`);
+                            tempEmails.push(...secondaryCrawledEmails);
+                        }
+                        
+                        broadcast(JSON.stringify({ name: businessName, url, emails: [...new Set(tempEmails)], platform: 'google'}));
+                        businessData.push({ name: businessName, url, emails: [...new Set(tempEmails)], platform: 'google'});
+
+                    } catch (error) {
+                        console.error(`Error navigating to ${url}: ${error}`);
+                    } finally {
+                        await newPage.close();
+                    }
+                }  
+            }
+
+            pageNumber = pageNumber+20
+        }
+
         console.log('this is the business data', businessData);
         const filteredBusinessData = businessData.filter(data => data.emails !== null);
         console.log('this is the filtered business data', filteredBusinessData);
         res.status(200).send('Scraping complete');
         await browser.close();
     
-    } catch (error) {
+    }catch(error) {
         console.error('Error:', error);
         broadcast('Error occurred: ' + error.message);
         res.status(500).send('Internal Server Error');
@@ -151,9 +236,6 @@ app.get('/', async(req, res) => {
     
         return crawledEmails;
     }
-    
-        
-    
 });
 
 
