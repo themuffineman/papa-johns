5c579482a6e82149924c987bcdb0e3371887124e
Petrus <petrusheya@gmail.com>
2024-04-04 11:49:11 +0200

    Added .env

--- google.js
+++ google.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require(dotenv).config()
+require('dotenv').config()
 const cors = require('cors')
 const puppeteer = require('puppeteer');
 const WebSocket = require('ws');
