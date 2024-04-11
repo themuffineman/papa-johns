74b33f0bd45e0dff087ca9674fe49735a1768f48
Petrus <petrusheya@gmail.com>
2024-04-11 18:37:53 +0200

    Added page.once

--- google.js
+++ google.js
@@ -61,7 +61,8 @@ app.get('/scrape', async(req, res) => {
         
             // Navigate to the GMB website
             if(intPageNumber === 0){
-                await page.goto(`https://www.google.com/localservices/prolist?g2lbs=AIQllVxEpXuuCPFrOHRAavT6nJMeIXUuM9D7r7-IlczaiEuKdgYVA09lqC7MIhZ3mUJ_MfwMM30K5vDmEB9UFLvwoZMUuqe_RIT2RmrDlIhrFndV8WuAgW-ioANkhbKSz__jtHfxKrJZLfFak9ca1Vbqi4HEnaKw7Q%3D%3D&hl=en-US&gl=&cs=1&ssta=1&q=${service}+in+${location}&oq=${service}+in+${location}&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0&slp=MgA6HENoTUkxWXZoamNfVmhBTVZZSUJRQmgxMkpBRTRSAggCYACSAZsCCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXd5YzRybWQKDC9nLzEycWg5dzhmZAoNL2cvMTFnNm5sMGxmNQoLL2cvMXRkY2dzdjQKCy9nLzF0aGwxODBzCgsvZy8xdGc3c2RmNwoLL2cvMXRkNGR6cTEKCy9nLzF0ZnNuZDRfCg0vZy8xMWI3bHBtOGIxCgsvZy8xdHp6dng1bAoLL2cvMXRrNHJsMTEKCy9nLzF0a3ZiNGpzCg0vZy8xMWJ4OGNteHM4Cg0vZy8xMWNuMF93MTkxCgsvZy8xdG15NWdzaAoLL2cvMXYzaF9jM3EKCy9nLzF2eWsyeHpnCgsvZy8xdGZtY24xcRIEEgIIARIECgIIAZoBBgoCFxkQAA%3D%3D&src=2&serdesk=1&sa=X&ved=2ahUKEwiyo9uNz9WEAxUMQkEAHZWwBcEQjGp6BAgfEAE`);
+                // await page.goto(`https://www.google.com/localservices/prolist?g2lbs=AIQllVxEpXuuCPFrOHRAavT6nJMeIXUuM9D7r7-IlczaiEuKdgYVA09lqC7MIhZ3mUJ_MfwMM30K5vDmEB9UFLvwoZMUuqe_RIT2RmrDlIhrFndV8WuAgW-ioANkhbKSz__jtHfxKrJZLfFak9ca1Vbqi4HEnaKw7Q%3D%3D&hl=en-US&gl=&cs=1&ssta=1&q=${service}+in+${location}&oq=${service}+in+${location}&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0&slp=MgA6HENoTUkxWXZoamNfVmhBTVZZSUJRQmgxMkpBRTRSAggCYACSAZsCCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXd5YzRybWQKDC9nLzEycWg5dzhmZAoNL2cvMTFnNm5sMGxmNQoLL2cvMXRkY2dzdjQKCy9nLzF0aGwxODBzCgsvZy8xdGc3c2RmNwoLL2cvMXRkNGR6cTEKCy9nLzF0ZnNuZDRfCg0vZy8xMWI3bHBtOGIxCgsvZy8xdHp6dng1bAoLL2cvMXRrNHJsMTEKCy9nLzF0a3ZiNGpzCg0vZy8xMWJ4OGNteHM4Cg0vZy8xMWNuMF93MTkxCgsvZy8xdG15NWdzaAoLL2cvMXYzaF9jM3EKCy9nLzF2eWsyeHpnCgsvZy8xdGZtY24xcRIEEgIIARIECgIIAZoBBgoCFxkQAA%3D%3D&src=2&serdesk=1&sa=X&ved=2ahUKEwiyo9uNz9WEAxUMQkEAHZWwBcEQjGp6BAgfEAE`);
+                await page.goto('https://www.google.com/localservices/prolist?g2lbs=AIQllVy8_BwcU5s9UwVyCtx14C0m8XSYJRnqCjpDSn8f1_V0I1sAIxnl7-RL40t8v15xdWPk3PmLf4jse7a_f5ET1nILbEqJ6s1FL_FpKVHzVmDVp2S4KZUqxOVAYPcsgyvdWt8K-AOL&hl=en-NA&gl=na&cs=1&ssta=1&q=architects%20in%20texas&oq=architects%20in%20texas&slp=MgBSAggCYACSAZsCCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXRkaDQ4aDMKCy9nLzF3eWM0cm1kCgsvZy8xdGtzajVkdwoML2cvMTJxaDl3OGZkCg0vZy8xMWc2bmwwbGY1CgsvZy8xdHQxdDJubgoLL2cvMXRobDE4MHMKCy9nLzF0ZGNnc3Y0Cg0vZy8xMWNteTRuOXY0CgsvZy8xdGQ0ZHpxMQoNL2cvMTFid3FiazB3ZgoLL2cvMXRmc25kNF8KCy9nLzF0Zjg3Z3dwCgsvZy8xdGc3c2RmNwoNL2cvMTFiN2xwbThiMQoLL2cvMXR6enZ4NWwKCy9nLzF0bXk1Z3NoCgsvZy8xdGtiNWhoMBIEEgIIARIECgIIAZoBBgoCFxkQAA%3D%3D&src=2&serdesk=1&sa=X&ved=2ahUKEwjL6pbpv7qFAxV90AIHHYWEDc8QjGp6BAglEAE&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0')
                 console.info('Navigated To GMB Website')
                 broadcast('Navigated To GMB Website')
 
@@ -71,61 +72,62 @@ app.get('/scrape', async(req, res) => {
                 await page.$eval('input#qjZKOb.MDhB7', input => input.value = '');
                 await page.type('input#qjZKOb.MDhB7', `${service} in ${location}`);
                 await page.keyboard.press('Enter');
-                await page.waitForNavigation({ waitUntil: 'networkidle0'})
-                await page.waitForSelector('span.VfPpkd-vQzf8d:has-text("< Previous")');
-                console.log('Previous Span has appeared')
-            
-                // Wait for cards to load
-                await page.waitForSelector('div.rgnuSb.xYjf2e');
-                await page.waitForSelector('.AIYI7d');
-              
-            
-                // Get all the business cards
-                const cards = await page.$$('div[jsname="gam5T"]');
-    
-                for (const card of cards) {
-                    const businessName = await card.$eval('div.rgnuSb.xYjf2e', node => node.textContent);
-                    const websiteATag = await card.$('a[aria-label="Website"]');
-                    const url = websiteATag ? await (await websiteATag.getProperty('href')).jsonValue() : null;
-            
-                    if (url) {
-                        const newPage = await browser.newPage();
-                        newPage.setDefaultNavigationTimeout(900000);
-                        newPage.setDefaultTimeout(900000);
-                    
-                        try {
-                            await newPage.goto(url);
-                            console.info(`Navigated to ${url}`);
-                            broadcast(`Navigated to ${url}`);
-    
+                page.once('framenavigated', async frame => {
+                    const newUrl = frame.url();
+                    console.log('Page has navigated to a new URL:', newUrl);
+
+                    // Wait for cards to load
+                    await page.waitForSelector('div.rgnuSb.xYjf2e');
+                    await page.waitForSelector('.AIYI7d');
+                  
+                
+                    // Get all the business cards
+                    const cards = await page.$$('div[jsname="gam5T"]');
         
-                            let tempEmails = []
-                            const crawledEmails = await crawl(newPage);
-                            tempEmails.push(...crawledEmails)
+                    for (const card of cards) {
+                        const businessName = await card.$eval('div.rgnuSb.xYjf2e', node => node.textContent);
+                        const websiteATag = await card.$('a[aria-label="Website"]');
+                        const url = websiteATag ? await (await websiteATag.getProperty('href')).jsonValue() : null;
+                
+                        if (url) {
+                            const newPage = await browser.newPage();
+                            newPage.setDefaultNavigationTimeout(900000);
+                            newPage.setDefaultTimeout(900000);
+                        
+                            try {
+                                await newPage.goto(url);
+                                console.info(`Navigated to ${url}`);
+                                broadcast(`Navigated to ${url}`);
         
-                            const rootUrl = new URL(url).hostname.replace(/^www\./, '');
-    
-                            //data pre-processing
-                            const anchorTags = await newPage.$$eval('a', anchors => anchors.map(anchor => anchor.href));
-                            const filteredAnchorTags = [...new Set(anchorTags)]; // removing duplicates
-                            const internalAnchorTags = filteredAnchorTags.filter((anchor)=> (anchor.includes(rootUrl))) // removing non root urls
-                            const internalLinks = internalAnchorTags.filter(link => (link.includes('contact'))); // filtering down to contact links
-                            
-                            for (const link of internalLinks) {
-                                await newPage.goto(link);
-                                const secondaryCrawledEmails = await crawl(newPage);
-                                tempEmails.push(...secondaryCrawledEmails);
+            
+                                let tempEmails = []
+                                const crawledEmails = await crawl(newPage);
+                                tempEmails.push(...crawledEmails)
+            
+                                const rootUrl = new URL(url).hostname.replace(/^www\./, '');
+        
+                                //data pre-processing
+                                const anchorTags = await newPage.$$eval('a', anchors => anchors.map(anchor => anchor.href));
+                                const filteredAnchorTags = [...new Set(anchorTags)]; // removing duplicates
+                                const internalAnchorTags = filteredAnchorTags.filter((anchor)=> (anchor.includes(rootUrl))) // removing non root urls
+                                const internalLinks = internalAnchorTags.filter(link => (link.includes('contact'))); // filtering down to contact links
+                                
+                                for (const link of internalLinks) {
+                                    await newPage.goto(link);
+                                    const secondaryCrawledEmails = await crawl(newPage);
+                                    tempEmails.push(...secondaryCrawledEmails);
+                                }
+                                
+                                broadcast(JSON.stringify({ name: businessName, url, emails: [...new Set(tempEmails)], platform: 'google'}));
+            
+                            } catch (error) {
+                                console.error(`Error navigating to ${url}: ${error}`);
+                            } finally {
+                                await newPage.close();
                             }
-                            
-                            broadcast(JSON.stringify({ name: businessName, url, emails: [...new Set(tempEmails)], platform: 'google'}));
-        
-                        } catch (error) {
-                            console.error(`Error navigating to ${url}: ${error}`);
-                        } finally {
-                            await newPage.close();
-                        }
-                    }  
-                }
+                        }  
+                    }
+                })
                 console.log('Scraping Complete')
                 res.status(200).send('Scraping complete');
             }
@@ -141,65 +143,70 @@ app.get('/scrape', async(req, res) => {
                 await page.$eval('input#qjZKOb.MDhB7', input => input.value = '');
                 await page.type('input#qjZKOb.MDhB7', `${service} in ${location}`);
                 await page.keyboard.press('Enter');
-                await page.waitForNavigation({ waitUntil: 'networkidle0'})
-                await page.waitForSelector('span.VfPpkd-vQzf8d:has-text("< Previous")');
-                console.log('Previous Span has appeared')
-
-
-                await page.goto(newUrl);
-        
-                // Wait for cards to load
-                const cards = await page.$$('div[jsname="gam5T"]');
-            
-                // max pages to scrape
-                const textContent = await page.$eval('.AIYI7d', element => element.textContent);
-                const lastNumbersMatch = textContent.match(/\d+$/);
-                const lastNumber = lastNumbersMatch ? parseInt(lastNumbersMatch[0], 10) : null;
-                const pagesToScrape = Math.floor(lastNumber / 20);
-                broadcast(JSON.stringify({pages:pagesToScrape}));
-    
-                for (const card of cards) {
-                    const businessName = await card.$eval('div.rgnuSb.xYjf2e', node => node.textContent);
-                    const websiteATag = await card.$('a[aria-label="Website"]');
-                    const url = websiteATag ? await (await websiteATag.getProperty('href')).jsonValue() : null;
-            
-                    if (url) {
-                        const newPage = await browser.newPage();
-                        newPage.setDefaultNavigationTimeout(900000);
-                        newPage.setDefaultTimeout(900000);
+                page.once('framenavigated', async frame => {
+                    const newUrl = frame.url();
+                    console.log('Page has navigated to a new URL:', newUrl);
+                    const editedUrl = newUrl + `&lci=${intPageNumber*20}`;
+                    console.log('this is the edited url', editedUrl)
+                    await page.goto(editedUrl);
+                    console.log('Now at the new site')
                     
-                        try {
-                            await newPage.goto(url);
-                            broadcast(`Navigated to ${url}`);
+                    await page.waitForSelector('div.rgnuSb.xYjf2e');
+                    await page.waitForSelector('.AIYI7d');
+            
+                    // Wait for cards to load
+                    const cards = await page.$$('div[jsname="gam5T"]');
+                
+                    // max pages to scrape
+                    const textContent = await page.$eval('.AIYI7d', element => element.textContent);
+                    const lastNumbersMatch = textContent.match(/\d+$/);
+                    const lastNumber = lastNumbersMatch ? parseInt(lastNumbersMatch[0], 10) : null;
+                    const pagesToScrape = Math.floor(lastNumber / 20);
+                    broadcast(JSON.stringify({pages:pagesToScrape}));
+        
+                    for (const card of cards) {
+                        const businessName = await card.$eval('div.rgnuSb.xYjf2e', node => node.textContent);
+                        const websiteATag = await card.$('a[aria-label="Website"]');
+                        const url = websiteATag ? await (await websiteATag.getProperty('href')).jsonValue() : null;
+                
+                        if (url) {
+                            const newPage = await browser.newPage();
+                            newPage.setDefaultNavigationTimeout(900000);
+                            newPage.setDefaultTimeout(900000);
+                        
+                            try {
+                                await newPage.goto(url);
+                                broadcast(`Navigated to ${url}`);
+        
+        
+                                let tempEmails = []
+                                const crawledEmails = await crawl(newPage);
+                                tempEmails.push(...crawledEmails)
+        
+                                const rootUrl = new URL(url).hostname.replace(/^www\./, '');;
+        
+                                // data pre-processing
+                                const anchorTags = await newPage.$$eval('a', anchors => anchors.map(anchor => anchor.href));
+                                const filteredAnchorTags = [...new Set(anchorTags)]; // removing duplicates
+                                const internalAnchorTags = filteredAnchorTags.filter((anchor)=> (anchor.includes(rootUrl))) // removing non root urls
+                                const internalLinks = internalAnchorTags.filter(link => (link.includes('contact'))); // filtering down to contact links
     
-    
-                            let tempEmails = []
-                            const crawledEmails = await crawl(newPage);
-                            tempEmails.push(...crawledEmails)
-    
-                            const rootUrl = new URL(url).hostname.replace(/^www\./, '');;
-    
-                            // data pre-processing
-                            const anchorTags = await newPage.$$eval('a', anchors => anchors.map(anchor => anchor.href));
-                            const filteredAnchorTags = [...new Set(anchorTags)]; // removing duplicates
-                            const internalAnchorTags = filteredAnchorTags.filter((anchor)=> (anchor.includes(rootUrl))) // removing non root urls
-                            const internalLinks = internalAnchorTags.filter(link => (link.includes('contact'))); // filtering down to contact links
-
-                            for (const link of internalLinks) {
-                                await newPage.goto(link);
-                                const secondaryCrawledEmails = await crawl(newPage);
-                                tempEmails.push(...secondaryCrawledEmails);
+                                for (const link of internalLinks) {
+                                    await newPage.goto(link);
+                                    const secondaryCrawledEmails = await crawl(newPage);
+                                    tempEmails.push(...secondaryCrawledEmails);
+                                }
+                                
+                                broadcast(JSON.stringify({ name: businessName, url, emails: [...new Set(tempEmails)], platform: 'google'}));
+        
+                            } catch (error) {
+                                console.error(`Error navigating to ${url}: ${error}`);
+                            } finally {
+                                await newPage.close();
                             }
-                            
-                            broadcast(JSON.stringify({ name: businessName, url, emails: [...new Set(tempEmails)], platform: 'google'}));
-    
-                        } catch (error) {
-                            console.error(`Error navigating to ${url}: ${error}`);
-                        } finally {
-                            await newPage.close();
-                        }
-                    }  
-                } 
+                        }  
+                    } 
+                });
                 res.status(200).send('Scraping complete');
                 console.log('Scraping Complete')
                 broadcast(`Finished Scraping Page ${intPageNumber}`)
