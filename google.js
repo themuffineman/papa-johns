798ef29c34a93f9b6e6588297c04ba08c4afe4de
Petrus <petrusheya@gmail.com>
2024-05-06 10:51:30 +0200

    Added if statement

--- google.js
+++ google.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 async function getLeads(){
-    console.log('Up and running')
+    console.log('Script is up and running')
     let browser;
     let startingCity
     let startingPage
@@ -18,8 +18,9 @@ async function getLeads(){
         const client = new MongoClient(process.env.URI)
         await client.connect()
         locationCollection = client.db('pendora').collection('locations')
-        intermidaryCollection = client.db('pendora').collection('intermidiary')
+        intermidaryCollection = client.db('pendora').collection('intermediary')
         const document = await locationCollection.findOne()
+        console.log('Heres the document', document)
 
         const cities = document.cities
         const service = 'Architects'
@@ -43,13 +44,14 @@ async function getLeads(){
         console.log('Puppeteer is launched')
 
 
-        while(emailsSent < 5){
+        while(emailsSent < 10){
            
             const page = await browser.newPage();
             page.setDefaultNavigationTimeout(900000); 
             page.setDefaultTimeout(900000);
 
-            while(startingPage < maxpages){
+            //Loop to scrape the whole city
+            while(startingPage < maxpages && emailsSent < 10){
 
                 if(startingPage === 0){
                     await page.goto(`https://www.google.com/localservices/prolist?g2lbs=AIQllVxEpXuuCPFrOHRAavT6nJMeIXUuM9D7r7-IlczaiEuKdgYVA09lqC7MIhZ3mUJ_MfwMM30K5vDmEB9UFLvwoZMUuqe_RIT2RmrDlIhrFndV8WuAgW-ioANkhbKSz__jtHfxKrJZLfFak9ca1Vbqi4HEnaKw7Q%3D%3D&hl=en-US&gl&cs=1&ssta=1&q=architects%20in%20texas&oq=architects%20in%20texas&slp=MgA6HENoTUkxWXZoamNfVmhBTVZZSUJRQmgxMkpBRTRSAggCYACSAZsCCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXd5YzRybWQKDC9nLzEycWg5dzhmZAoNL2cvMTFnNm5sMGxmNQoLL2cvMXRkY2dzdjQKCy9nLzF0aGwxODBzCgsvZy8xdGc3c2RmNwoLL2cvMXRkNGR6cTEKCy9nLzF0ZnNuZDRfCg0vZy8xMWI3bHBtOGIxCgsvZy8xdHp6dng1bAoLL2cvMXRrNHJsMTEKCy9nLzF0a3ZiNGpzCg0vZy8xMWJ4OGNteHM4Cg0vZy8xMWNuMF93MTkxCgsvZy8xdG15NWdzaAoLL2cvMXYzaF9jM3EKCy9nLzF2eWsyeHpnCgsvZy8xdGZtY24xcRIEEgIIARIECgIIAZoBBgoCFxkQAA%3D%3D&src=2&serdesk=1&sa=X&ved=2ahUKEwiyo9uNz9WEAxUMQkEAHZWwBcEQjGp6BAgfEAE&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0`);
@@ -221,9 +223,11 @@ async function getLeads(){
                 console.log('Max Pages is:', maxpages)
                 console.log('Starting Page is now', startingPage)
             }
-            startingCity++ 
-            startingPage = 0
-            maxpages = 1
+            if(startingPage >= maxpages){
+                startingCity++ 
+                startingPage = 0
+                maxpages = 1
+            }
             page.close()
             console.log('Starting City is now', startingCity,'/', cities[startingCity])
         }
@@ -231,7 +235,8 @@ async function getLeads(){
     }catch(error) { 
         console.error('Error Occured:', error);
     }finally{
-        startingCity && startingPage && await locationCollection.updateOne({ $set: { index: startingCity, page: startingPage }})
+        await locationCollection.updateOne({ $set: { index: startingCity, page: startingPage }})
+        console.log('Reached Max Emails')
         await browser.close();
     }
 
@@ -270,6 +275,3 @@ async function crawl(page) {
 }
 
 getLeads()
-
-
-
