b1777c33a5b140976e592e5049049c80c45ecade
Petrus <petrusheya@gmail.com>
2024-05-08 21:19:01 +0200

    Added express

--- google.js
+++ google.js
@@ -1,9 +1,18 @@
 import {MongoClient, ObjectId} from 'mongodb'
 import puppeteer from 'puppeteer'
+import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 
-async function getLeads(){
+const PORT = 6969
+
+const app = express()
+
+app.listen(PORT, ()=> {console.log(`Server listening on port ${PORT}`)})
+
+app.get('/leads', async (req , res)=>{
+    
+    
     console.log('Script is up and running')
     let browser;
     let startingCity
@@ -48,13 +57,13 @@ async function getLeads(){
 
 
         //Loop to scrape the whole city
-        while(emailsSent < 10){
-           
+        while(emailsSent < 100){
+            
             const page = await browser.newPage();
             page.setDefaultNavigationTimeout(900000); 
             page.setDefaultTimeout(900000);
 
-            while(startingPage < maxpages && emailsSent < 10){
+            while(startingPage < maxpages && emailsSent < 100){
 
                 if(startingPage === 0){
                     await page.goto(`https://www.google.com/localservices/prolist?g2lbs=AIQllVxEpXuuCPFrOHRAavT6nJMeIXUuM9D7r7-IlczaiEuKdgYVA09lqC7MIhZ3mUJ_MfwMM30K5vDmEB9UFLvwoZMUuqe_RIT2RmrDlIhrFndV8WuAgW-ioANkhbKSz__jtHfxKrJZLfFak9ca1Vbqi4HEnaKw7Q%3D%3D&hl=en-US&gl&cs=1&ssta=1&q=architects%20in%20texas&oq=architects%20in%20texas&slp=MgA6HENoTUkxWXZoamNfVmhBTVZZSUJRQmgxMkpBRTRSAggCYACSAZsCCgsvZy8xdGg2ZjZ4ZwoNL2cvMTFoY3c1ZDltZAoLL2cvMXd5YzRybWQKDC9nLzEycWg5dzhmZAoNL2cvMTFnNm5sMGxmNQoLL2cvMXRkY2dzdjQKCy9nLzF0aGwxODBzCgsvZy8xdGc3c2RmNwoLL2cvMXRkNGR6cTEKCy9nLzF0ZnNuZDRfCg0vZy8xMWI3bHBtOGIxCgsvZy8xdHp6dng1bAoLL2cvMXRrNHJsMTEKCy9nLzF0a3ZiNGpzCg0vZy8xMWJ4OGNteHM4Cg0vZy8xMWNuMF93MTkxCgsvZy8xdG15NWdzaAoLL2cvMXYzaF9jM3EKCy9nLzF2eWsyeHpnCgsvZy8xdGZtY24xcRIEEgIIARIECgIIAZoBBgoCFxkQAA%3D%3D&src=2&serdesk=1&sa=X&ved=2ahUKEwiyo9uNz9WEAxUMQkEAHZWwBcEQjGp6BAgfEAE&scp=Cg5nY2lkOmFyY2hpdGVjdBJMEhIJSTKCCzZwQIYRPN4IGI8c6xYaEgkLNjLkhLXqVBFCt95Dkrk7HCIKVGV4YXMsIFVTQSoUDV1uZg8VcypvwB3BkMEVJTvOQ8gwABoKYXJjaGl0ZWN0cyITYXJjaGl0ZWN0cyBpbiB0ZXhhcyoJQXJjaGl0ZWN0`);
@@ -86,7 +95,7 @@ async function getLeads(){
                     // Wait for cards to load
                     await page.waitForSelector('div.rgnuSb.xYjf2e');
                     await page.waitForSelector('.AIYI7d');
-                  
+                    
                 
                     // Get all the business cards
                     const cards = await page.$$('div[jsname="gam5T"]');
@@ -250,42 +259,41 @@ async function getLeads(){
         console.log(result.acknowledged)
         await browser.close();
     }
-
-}
-  
-async function crawl(page) {
-    const crawledEmails = [];
-
-    // Extract all <a> tags on the page
-    const links = await page.evaluate(() => {
-        const anchorTags = Array.from(document.querySelectorAll('a'));
-        return anchorTags.map(a => a.href);
-    });
-
-    const emailLinks = links.filter(link => link.startsWith('mailto:'));
-
-    emailLinks.forEach(link => {
-        const email = link.replace('mailto:', '');
-        crawledEmails.push(email);
-    });
-
-    const text = await page.evaluate(() => document.body.textContent);
-
-    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-
-    let match;
-    while ((match = emailRegex.exec(text)) !== null) {
-        crawledEmails.push(match[0]);
+    
+    async function crawl(page) {
+        const crawledEmails = [];
+    
+        // Extract all <a> tags on the page
+        const links = await page.evaluate(() => {
+            const anchorTags = Array.from(document.querySelectorAll('a'));
+            return anchorTags.map(a => a.href);
+        });
+    
+        const emailLinks = links.filter(link => link.startsWith('mailto:'));
+    
+        emailLinks.forEach(link => {
+            const email = link.replace('mailto:', '');
+            crawledEmails.push(email);
+        });
+    
+        const text = await page.evaluate(() => document.body.textContent);
+    
+        const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+    
+        let match;
+        while ((match = emailRegex.exec(text)) !== null) {
+            crawledEmails.push(match[0]);
+        }
+    
+        const finalEmails = crawledEmails.filter((email)=>{
+            return email != undefined && email != null
+        })
+        const trulyFinalEmails = finalEmails.filter((email)=>{
+            return !email.includes("wixpress")
+        })
+    
+        return trulyFinalEmails; //better name TBD
     }
+})
 
-    const finalEmails = crawledEmails.filter((email)=>{
-        return email != undefined && email != null
-    })
-    const trulyFinalEmails = finalEmails.filter((email)=>{
-        return !email.includes("wixpress")
-    })
 
-    return trulyFinalEmails; //better name TBD
-}
-
-getLeads()
